b2766c1a197ea53c14fb51549bacc2c954731990
Jason Huang <f230536@hotmail.com>
2016-01-12 02:01:47 -0800

    update

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 #!flask/bin/python
+import os
+from flask import Flask
 from app import app
-app.run(debug=True)
+
+
+if __name__ == "__main__":
+	# app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 # app.run('24.6.153.190')
 # app.run(host='192.168.1.255', port=5000)
